98c1d427d0b14ae6ed20279b13ed6c0c825be74c
rodhiyanto11 <rodhi.rodhiyanto@gmail.com>
2020-04-09 09:17:29 +0700

    update

---
+++ src/component/Layout/Layout.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+import Navbar from '../Navbar/Navbar';
+import Home from '../../pages/Home/Home';
+import Contact from '../../pages/Contact/Contact';
+import Project from '../../pages/Project/Project';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+export default class Layout extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Navbar />
+                    <Switch>
+                        <Route exact path="/home" component={Home} />
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/contact" component={Contact} />
+                        <Route exact path="/project" component={Project} />
+                    </Switch>
+                </Router>
+
+                
+
+
+                <footer class="site-footer">
+                    <div class="container">
+                        <div class="row">
+
+                            <div class="col-lg-5 mx-lg-auto col-md-8 col-10">
+                                <h1 class="text-white" data-aos="fade-up" data-aos-delay="100">We make creative <strong>brands</strong> only.</h1>
+                            </div>
+
+                            <div class="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="200">
+                                <h4 class="my-4">Contact Info</h4>
+
+                                <p class="mb-1">
+                                    <i class="fa fa-phone mr-2 footer-icon"></i>
+         +99 080 070 4224
+       </p>
+
+                                <p>
+                                    <a href="#">
+                                        <i class="fa fa-envelope mr-2 footer-icon"></i>
+           hello@company.com
+         </a>
+                                </p>
+                            </div>
+
+                            <div class="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="300">
+                                <h4 class="my-4">Our Studio</h4>
+
+                                <p class="mb-1">
+                                    <i class="fa fa-home mr-2 footer-icon"></i>
+         Av. Lúcio Costa - Barra da Tijuca, Rio de Janeiro - RJ, Brazil
+       </p>
+                            </div>
+
+                            <div class="col-lg-4 mx-lg-auto text-center col-md-8 col-12" data-aos="fade-up" data-aos-delay="400">
+                                <p class="copyright-text">Copyright &copy; 2020 Your Company
+       <br />
+                                </p>
+                            </div>
+
+                            <div class="col-lg-4 mx-lg-auto col-md-6 col-12" data-aos="fade-up" data-aos-delay="500">
+
+                                <ul class="footer-link">
+                                    <li><a href="#">Stories</a></li>
+                                    <li><a href="#">Work with us</a></li>
+                                    <li><a href="#">Privacy</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="col-lg-3 mx-lg-auto col-md-6 col-12" data-aos="fade-up" data-aos-delay="600">
+                                <ul class="social-icon">
+                                    <li><a href="#" class="fa fa-instagram"></a></li>
+                                    <li><a href="#" class="fa fa-twitter"></a></li>
+                                    <li><a href="#" class="fa fa-dribbble"></a></li>
+                                    <li><a href="#" class="fa fa-behance"></a></li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                </footer>
+
+
+
+            </div>
+        )
+    }
+}
---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Home from '../../pages/Home/Home';
+import Contact from '../../pages/Contact/Contact';
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <nav class="navbar navbar-expand-lg">
+                    <div class="container">
+                        <a class="navbar-brand" href="index.html">
+                            <i class="fa fa-line-chart"></i>
+                                The Card Us
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        
+                            <div class="collapse navbar-collapse" id="navbarNav">
+                                <ul class="navbar-nav ml-auto">
+                                   
+                                    <li class="nav-item">
+                                        <Link class="nav-link smoothScroll" to="/home">Home</Link>
+                                    </li>
+                                    <li class="nav-item">
+                                        <Link class="nav-link smoothScroll" to="/contact">Contact</Link>
+                                    </li>
+                                    <li class="nav-item">
+                                        <Link class="nav-link smoothScroll" to="/project">Portofolio</Link>
+                                    </li>
+                                    {/* <li class="nav-item">
+                                        <a href="#about" class="nav-link smoothScroll">Studio</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#project" class="nav-link smoothScroll">Our Works</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="blog.html" class="nav-link">Blog</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="contact.html" class="nav-link contact">Contact</a>
+                                    </li> */}
+                                </ul>
+                              
+                            </div>
+                       
+                   </div>
+                   </nav>
+
+            </div>
+        )
+    }
+}
